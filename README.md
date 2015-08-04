cfc881536b4aa80952c8a93e23c4d5b90d6dc7fc
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-08-04 15:46:43 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Website Futurality Theme
+==================
+
+Futurality Theme For the website
+
+
+Features
+========
+* Simplifies the website
---
+++ __openerp__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#   Copyright (c) 2015- Oy Tawasta Technologies Ltd. (http://www.tawasta.fi)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Website Futurality Theme',
+    'category': 'Website',
+    'version': '0.1',
+    'author': 'Oy Tawasta Technologies Ltd',
+    'website': 'http://www.tawasta.fi',
+    'depends': [
+        'website',
+    ],
+    'description': '''
+Website Futurality Theme
+==================
+
+Futurality Theme For the website
+
+
+Features
+========
+* Simplifies the website
+''',
+    'data': [
+    ],
+}
