3b2bacfc98c51710bfa995d34a21582bdc8ded15
Aleksi Savijoki <aleksi.savijoki@tawasta.fi>
2015-12-08 14:14:36 +0200

    Fixed a bug, where the information was retrieved from res.partner public user when not signed in.

--- controllers/main.py
+++ controllers/main.py
@@ -34,20 +34,41 @@ class website_sale(website_sale):
 		orm_partner = registry.get('res.partner')
 		orm_user = registry.get('res.users')
 		partner = orm_user.browse(cr, SUPERUSER_ID, request.uid, context).partner_id
+		order = request.website.sale_get_order(force_create=1, context=context)
 
-		#Partneriin kirjoitus
-		partner.staff_count = request.params['staff_count']
+		checkout['staff_count'] = request.params['staff_count']
+		checkout['member_privacy'] = request.params['member_privacy']
+		checkout['steering_member'] = request.params['steering_member']
 
-		if 'member_privacy' in request.params:
-			partner.member_privacy = request.params['member_privacy']
+
+		partner_lang = request.lang if request.lang in [lang.code for lang in request.website.language_ids] else None
+
+		billing_info = {'customer': True}
+		if partner_lang:
+			billing_info['lang'] = partner_lang
+
+		billing_info.update(self.checkout_parse('billing', checkout, True))
+
+		# set partner_id
+		partner_id = None
+		if request.uid != request.website.user_id.id:
+			partner_id = orm_user.browse(cr, SUPERUSER_ID, uid, context=context).partner_id.id
+		elif order.partner_id:
+			user_ids = request.registry['res.users'].search(cr, SUPERUSER_ID,
+				[("partner_id", "=", order.partner_id.id)], context=dict(context or {}, active_test=False))
+			if not user_ids or request.website.user_id.id not in user_ids:
+				partner_id = order.partner_id.id
+
+
+		# save partner informations
+		if partner_id and request.website.partner_id.id != partner_id:
+			orm_partner.write(cr, SUPERUSER_ID, [partner_id], billing_info, context=context)
 		else:
-			partner.member_privacy = False
+			# create partner
+			partner_id = orm_partner.create(cr, SUPERUSER_ID, billing_info, context=context)
+			print partner_id
 
-
-		if 'steering_member' in request.params:
-			partner.steering_member = request.params['steering_member']
-		else:
-			partner.steering_member = False
+		
 
 	# Get the new fields from partner for to checkout
 	def checkout_values(self, data=None):
@@ -57,6 +78,7 @@ class website_sale(website_sale):
 		partner = orm_user.browse(cr, SUPERUSER_ID, request.uid, context).partner_id
 		values = super(website_sale, self).checkout_values(data)
 
+		# print partner.id
 		values['checkout']['member_privacy'] = partner.member_privacy
 		values['checkout']['staff_count'] = partner.staff_count
 		values['checkout']['steering_member'] = partner.steering_member
