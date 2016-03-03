c125b50cf22e09c83d0709fc1fd537f8c392df80
Aleksi Savijoki <aleksi.savijoki@tawasta.fi>
2016-03-03 11:33:33 +0200

    Added fields to optional + mandatory fields, so they get written to created partner.

--- __openerp__.py
+++ __openerp__.py
@@ -22,7 +22,7 @@
 {
     'name': 'eCommerce Extension',
     'summary': 'Checkout form modifications',
-    'version': '8.0.0.7.0',
+    'version': '8.0.0.7.1',
     'category': 'Website',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
