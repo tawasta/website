d449f8a86634ce5ad765859838f771a1337d7c20
Aleksi Savijoki <aleksi.savijoki@tawasta.fi>
2016-04-07 13:57:05 +0300

    Change version number

--- __openerp__.py
+++ __openerp__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Event Extension',
     'summary': 'Changes to Event app',
-    'version': '8.0.0.1.2',
+    'version': '8.0.0.1.3',
     'category': 'Marketing',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
