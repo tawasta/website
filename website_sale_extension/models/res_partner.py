84390835c01a7bb467833ec045ecdbc0a991f945
Aleksi Savijoki <aleksi.savijoki@tawasta.fi>
2016-07-27 15:12:30 +0300

    Created company_name field for res_partner. Created field to checkout for the same purpose (shown to personal customers).

---
+++ website_sale_extension/models/res_partner.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+# 1. Standard library imports:
+
+# 2. Known third party imports:
+
+# 3. Odoo imports (openerp):
+from openerp import api, fields, models
+
+# 4. Imports from Odoo modules:
+
+# 5. Local imports in the relative form:
+
+# 6. Unknown third party imports:
+
+class ResPartner(models.Model):
+
+	# 1. Private attributes
+    _inherit = 'res.partner'
+
+    # 2. Fields declaration
+    company_name = fields.Char(string='Company name')
+    
+    # 3. Default methods
+
+    # 4. Compute and search fields, in the same order that fields declaration
+
+    # 5. Constraints and onchanges
+
+    # 6. CRUD methods
+
+    # 7. Action methods
+
+    # 8. Business methods
