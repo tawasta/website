5463350ca184261a78f6447c6a3823a6bca1deb0
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2016-08-11 15:40:18 +0300

    Add name splitting

--- website_sale_extension/__openerp__.py
+++ website_sale_extension/__openerp__.py
@@ -22,7 +22,7 @@
 {
     'name': 'eCommerce Extension',
     'summary': 'Checkout form modifications',
-    'version': '8.0.0.7.10',
+    'version': '8.0.0.7.11',
     'category': 'Website',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
--- website_sale_extension/static/src/js/checkout_form.js
+++ website_sale_extension/static/src/js/checkout_form.js
@@ -1,12 +1,54 @@
 $(document).ready(function(){
 
+    // Automatically format the name field
+    $("#forename_input").keyup(function(){
+        updateName();
+    });
+    $("#surname_input").keyup(function(){
+        updateName();
+    });
+
+    function updateName(){
+        var full_name = $("#forename_input").val() + " " + $("#surname_input").val()
+
+        $('#name_input').val(full_name);
+    };
+
+    // Set the private customer name if reloaded
+    setNameInputs();
+
+    function setNameInputs(){
+        // This splitting is pretty terrible, but will work most of the times
+        var names = $('#name_input').val().split(" ");
+
+        var forename = ""
+        var surname = ""
+        jQuery.each(names, function(index, item) {
+            console.log( index );
+            console.log( item );
+            if(index == 0){
+                forename = item;
+                return true;
+            }
+
+            if(index > 1){
+                surname += " ";
+            }
+            surname += item;
+        });
+
+        $("#forename_input").val(forename);
+        $("#surname_input").val(surname);
+    }
+
     $('#personal_customer').click(function() {
         // If personal customer is checked, SHOW these
-        $("#contact_label").toggle(this.checked);
+        $("#contact_name_div").toggle(this.checked);
         $("#title_div").toggle(this.checked);
         $("#organization_div").toggle(this.checked);
 
         // If personal customer is checked, HIDE these
+        $("#name_input").toggle(!this.checked);
         $("#businessid_div").toggle(!this.checked);
         $("#businessid_temp_div").toggle(!this.checked);
         $("#website_div").toggle(!this.checked);
