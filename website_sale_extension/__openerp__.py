d62fe7ca32fa4722986dd0605822fda82ae7f1e9
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2016-08-16 15:06:51 +0300

    Improve used terms

--- website_sale_extension/__openerp__.py
+++ website_sale_extension/__openerp__.py
@@ -22,7 +22,7 @@
 {
     'name': 'eCommerce Extension',
     'summary': 'Checkout form modifications',
-    'version': '8.0.0.7.14',
+    'version': '8.0.0.7.15',
     'category': 'Website',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
