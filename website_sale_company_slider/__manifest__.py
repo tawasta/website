27b458b8a7ab68580478d2b9f17e49c109e74d31
Aleksi Savijoki <aleksi.savijoki@tawasta.fi>
2018-11-21 17:48:11 +0200

    Update js-file version number

--- website_sale_company_slider/__manifest__.py
+++ website_sale_company_slider/__manifest__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Website Sale Company Slider',
     'summary': 'Slider to checkout',
-    'version': '10.0.1.5.0',
+    'version': '10.0.1.5.1',
     'category': 'Website',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
