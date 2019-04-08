61e3571c416a2d5c4899df0845e5988e93c0d384
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2019-04-08 12:53:26 +0300

    Refactor to 12.0

--- website_unread_messages/__manifest__.py
+++ website_unread_messages/__manifest__.py
@@ -22,13 +22,13 @@
 {
     'name': 'Website Unread Messages',
     'summary': 'Unread messages for website',
-    'version': '10.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Website',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
     'license': 'AGPL-3',
     'application': False,
-    'installable': False,
+    'installable': True,
     'depends': [
         'website',
         'website_utilities',
