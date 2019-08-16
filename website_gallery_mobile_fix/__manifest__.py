e2b6fa0935f8e5b1d49e3fd5f1917b5d7747d3c2
retropikzel <retropikzel@mailbox.org>
2019-08-16 13:19:47 +0300

    [ADD] Added new module

---
+++ website_gallery_mobile_fix/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': "website_gallery_mobile_fix",
+    'summary': "Fixes odoo website gallery mobile view",
+    'description': """
+        - Fix images having nospace between in gallery grid mode in mobile""",
+    'author': 'Oy Tawasta OS Technologies Ltd.',
+    'license': 'AGPL-3',
+    'website': 'http://www.tawasta.fi',
+    'category': 'Website',
+    'version': '12.0.1.0.0',
+    'depends': [
+        'website',
+    ],
+    'data': [
+        'views/assets_frontend.xml',
+    ],
+    'demo': [],
+}
