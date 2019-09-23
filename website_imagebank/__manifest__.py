90da723fcf1dd672ac02bf273c675e91dda50275
retropikzel <retropikzel@mailbox.org>
2019-09-23 15:06:29 +0300

    [FIX] Renamed module

---
+++ website_imagebank/__manifest__.py
@@ -0,0 +1,29 @@
+{
+    'name': "Website Image Bank",
+    'summary': """
+Allow website visitors to upload images to website gallery.
+    """,
+    'description': """
+- Allow website visitors to upload pictures
+- Curate pictures before publishing
+    """,
+    'website': 'http://www.tawasta.fi',
+    'author': 'Oy Tawasta Technologies Ltd.',
+    'license': 'AGPL-3',
+    'category': 'Website',
+    'version': '12.0.1.0.0',
+    'application': True,
+    'depends': [
+        'website',
+        'website_form_recaptcha',
+        'website_gallery_mobile_fix',
+    ],
+    'data': [
+        'views/views.xml',
+        'views/templates.xml',
+        'data/menu.xml',
+        'security/ir.model.access.csv',
+        'data/data.xml',
+    ],
+    'demo': [],
+}
---
+++ website_imagebank/models/models.py
@@ -0,0 +1,52 @@
+from odoo import models, fields, api
+
+class VisitorImageCategory(models.Model):
+    _name = 'imagebank.category'
+    name = fields.Char(string='Name')
+
+class WebsiteVisitorImage(models.Model):
+    _name = 'imagebank.image'
+    _description = 'Image uploaded by website visitor'
+
+    name = fields.Char(string="Name")
+    category = fields.Many2one(string="Category", comodel_name='imagebank.category', ondelete='restrict')
+    website_published = fields.Boolean(string="Published on website", default=False)
+    active = fields.Boolean(default=True)
+    image_url = fields.Char(string="Image URL")
+    filename = fields.Char(string="Filename")
+    attachment = fields.Many2one(string="Attachment Image", comodel_name="ir.attachment", ondelete="cascade")
+    attachment_image = fields.Binary(related="attachment.datas")
+
+    def get_category_list(self):
+        categories = []
+        for category in self.env['imagebank.category'].search([]):
+            categories.append(category.name)
+        return categories
+
+    def get_image_urls_by_category(self):
+        ret = []
+        for category in self.env['imagebank.category'].search([]):
+            image_urls = [category.name]
+            for image in self.search([('category.id', '=', category.id), ('website_published', '=', True)]):
+                image_urls.append(image.image_url)
+            ret.append(image_urls)
+        return ret
+
+    @api.model
+    def get_published_urls(self):
+        image_urls = []
+        for image in self.search([('website_published', '=', True)]):
+            image_urls.append(image.image_url)
+        return image_urls
+
+    def toggle_website_published(self):
+        self.website_published = not self.website_published
+
+    @api.model
+    def create(self, vals):
+        super(WebsiteVisitorImage, self).create(vals)
+
+    def unlink(selfs):
+        for self in selfs:
+            self.attachment.unlink()
+            super(WebsiteVisitorImage, self).unlink()
