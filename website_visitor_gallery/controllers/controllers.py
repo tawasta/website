f477c6f91c4b597ab3750bf81c95f1083f55a21c
retropikzel <retropikzel@mailbox.org>
2019-09-23 11:21:05 +0300

    [FIX] Dont show empty categories in gallery page

--- website_visitor_gallery/controllers/controllers.py
+++ website_visitor_gallery/controllers/controllers.py
@@ -12,6 +12,7 @@ class VisitorImgUpload(http.Controller):
     @http.route('/visitor_gallery', auth='public', type='http', website=True)
     def visitor_gallery(self, **kw):
         categories = http.request.env['visitor.image'].get_category_list()
+        print(categories)
         image_urls = http.request.env['visitor.image'].get_published_urls()
         image_urls_by_category = \
             http.request.env['visitor.image'].get_image_urls_by_category()
@@ -25,31 +26,34 @@ class VisitorImgUpload(http.Controller):
     def create_slide(self, *args, **post):
         if post.get('image'):
             try:
-                Attachment = http.request.env['ir.attachment'].sudo()
-                VisitorImage = http.request.env['visitor.image'].sudo()
+                for img_filestorage in http.request.httprequest.files.getlist('image'):
 
-                img = post.get('image').read()
-                img_optimized = self.optimize_img(img)
-                datas = base64.b64encode(img_optimized)
-                filename = post.get('image').filename
-                category = http.request.env['vimage.category'].search(
-                    [('name', '=', post.get('category'))])
+                    Attachment = http.request.env['ir.attachment'].sudo()
+                    VisitorImage = http.request.env['visitor.image'].sudo()
 
-                attachment = Attachment.create({
-                    'name': filename,
-                    'datas': datas,
-                    'type': 'binary',
-                    'datas_fname': filename,
-                    'public': True,
-                })
+                    img = img_filestorage.read() #post.get('image').read()
+                    img_optimized = self.optimize_img(img)
+                    datas = base64.b64encode(img_optimized)
+                    filename = img_filestorage.filename
+                    category = http.request.env['vimage.category'].search(
+                        [('name', '=', post.get('category'))])
 
-                VisitorImage.create({
-                    'name': filename,
-                    'filename': post.get('image').filename,
-                    'attachment': attachment.id,
-                    'image_url': "/web/image/" + str(attachment.id) + "/" + filename,
-                    'category': category.id,
-                })
+                    attachment = Attachment.create({
+                        'name': filename,
+                        'datas': datas,
+                        'type': 'binary',
+                        'datas_fname': filename,
+                        'public': True,
+                    })
+
+                    VisitorImage.create({
+                        'name': filename,
+                        'filename': filename,
+                        'attachment': attachment.id,
+                        'image_url': "/web/image/" + str(attachment.id) + "/" + filename,
+                        'category': category.id,
+                    })
+
                 return http.request.render('website_visitor_gallery.visitor_gallery_thank_you', {})
             except Exception as e:
                 print(e)
