51b10b318c3670b8cc1ee2667976299d8ba809b5
retropikzel <retropikzel@mailbox.org>
2019-11-12 13:06:24 +0200

    [ADD] Adding module

---
+++ website_sale_hide_product_default_info/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': "Website sale product hide default info",
+    'summary': """ """,
+    'description': """ """,
+    'website': 'http://www.tawasta.fi',
+    'author': 'Oy Tawasta Technologies Ltd.',
+    'license': 'AGPL-3',
+    'category': 'Website',
+    'version': '12.0.1.0.0',
+    'depends': [
+        'website',
+        'website_sale'
+    ],
+    'data': [
+        'views/assets.xml'
+    ],
+    'demo': [],
+}
