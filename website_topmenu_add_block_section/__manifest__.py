632784f25f3032295c67c3ceaecf2d072223fdd8
retropikzel <retropikzel@mailbox.org>
2020-01-14 15:48:48 +0200

    [ADD] New module

---
+++ website_topmenu_add_block_section/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Website Top Menu Block Section',
+    'summary': 'Adds section to bottom of top menu to add website blocks into',
+    'description': '',
+    'author': 'Oy Tawasta OS Technologies Ltd.',
+    'license': 'AGPL-3',
+    'website': 'http://www.tawasta.fi',
+    'category': 'Website',
+    'version': '12.0.1.0.0',
+    'depends': [
+        'website',
+    ],
+    'data': [
+        'views/menu.xml',
+    ],
+    'demo': [],
+}
