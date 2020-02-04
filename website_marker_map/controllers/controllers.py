f7701d800118f3dbc84bd01325024cd1577fad6a
retropikzel <retropikzel@mailbox.org>
2020-02-04 13:01:12 +0200

    [ADD] New module

---
+++ website_marker_map/controllers/controllers.py
@@ -0,0 +1,34 @@
+from odoo import http
+from odoo.http import request
+from odoo.http import route
+import json
+
+import logging
+_logger = logging.getLogger(__name__)
+
+class MarkerMapExample(http.Controller):
+    @route('/example', auth='public')
+    def state_example_handler(self):
+        map_data = {
+            'settings': {
+                'zoom': 5,
+                'center': {
+                    'lat': 65.5274,
+                    'lng': 27.1588
+                }
+            },
+            'markers': [
+                {
+                    'position': {
+                        'lat': 60.8701996,
+                        'lng': 26.7018042,
+
+                    },
+                    'label': "A",
+                    'show_infowindow': True,
+                    'infowindow_open_start': False,
+                    'infowindow_text': '<h5>Info</h5>'
+                }
+            ]
+        }
+        return json.dumps(map_data)
---
+++ website_marker_map/static/src/js/map.js
@@ -0,0 +1,51 @@
+function initMap() {
+    $.get( "/" + window.location.hash.replace("#", "") , function( data ) {
+
+        data = JSON.parse(data);
+
+        console.log(data);
+        var settings = {
+            zoom: 5,
+            center: {
+                lat: 65.5274,
+                lng: 27.1588
+            }
+        };
+        if(data.settings) {
+            settings = data.settings;
+        }
+
+        var map = new google.maps.Map(document.getElementById('map'), settings);
+
+        data.markers.forEach(function(marker_data) {
+
+            marker_data.map = map;
+            var marker = new google.maps.Marker(marker_data);
+
+            if(marker_data.show_infowindow == true) {
+
+                var infowindow = new google.maps.InfoWindow({
+                    content: marker_data.infowindow_text
+                });
+
+                marker.addListener('click', function() {
+                    infowindow.open(map, marker);
+                });
+
+                if(marker_data.infowindow_open_start == true) {
+                    infowindow.open(map, marker);
+                }
+            }
+        });
+    });
+}
+
+$(window).hashchange(initMap);
+
+$( document ).ready(function() {
+    initMap();
+});
+
+
+
+
