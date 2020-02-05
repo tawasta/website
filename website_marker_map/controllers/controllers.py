4b9d1f15e225bb6ffef4f7c29460d593e3ae8172
retropikzel <retropikzel@mailbox.org>
2020-02-05 14:30:29 +0200

    [FIX] Flake8

--- website_marker_map/controllers/controllers.py
+++ website_marker_map/controllers/controllers.py
@@ -1,6 +1,6 @@
 from odoo import http
-from odoo.http import request, route
 import json
+
 
 class your_class(http.Controller):
     @http.route('/marker_map', type='http', auth='public', website=True)
@@ -8,8 +8,9 @@ class your_class(http.Controller):
         return http.request.render(
             'website_marker_map.marker_map_page', {})
 
+
 class Example(http.Controller):
-    @route('/marker_map_api/example', auth='public')
+    @http.route('/marker_map_api/example', auth='public')
     def state_example_handler(self):
         map_data = {
             'settings': {
