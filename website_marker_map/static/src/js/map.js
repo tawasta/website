fcde7d9f86ab4048f6703ceb70748fe1bed1d9e8
retropikzel <retropikzel@mailbox.org>
2020-02-05 15:08:16 +0200

    [FIX] eslint

--- website_marker_map/static/src/js/map.js
+++ website_marker_map/static/src/js/map.js
@@ -1,55 +1,57 @@
-function initMap() {
+(function markerMap () {
+    'use strict';
+    function initMap () {
 
-    var api_url = "/marker_map_api/" + window.location.hash.replace("#", "");
+        var api_url = "/marker_map_api/" + window.location.hash.replace("#", "");
 
-    console.log(api_url);
-    $.get(api_url, function(data){
+        console.log(api_url);
+        $.get(api_url, function (data) {
 
-        data = JSON.parse(data);
+            parsed_data = JSON.parse(data);
 
-        console.log(data);
-        var settings = {
-            zoom: 5,
-            center: {
-                lat: 65.5274,
-                lng: 27.1588
-            }
-        };
-        if(data.settings) {
-            settings = data.settings;
-        }
-
-        var map = new google.maps.Map(document.getElementById('marker_map'), settings);
-
-        data.markers.forEach(function(marker_data) {
-
-            marker_data.map = map;
-            var marker = new google.maps.Marker(marker_data);
-
-            if(marker_data.show_infowindow == true) {
-
-                var infowindow = new google.maps.InfoWindow({
-                    content: marker_data.infowindow_text
-                });
-
-                marker.addListener('click', function() {
-                    infowindow.open(map, marker);
-                });
-
-                if(marker_data.infowindow_open_start == true) {
-                    infowindow.open(map, marker);
+            var settings = {
+                zoom: 5,
+                center: {
+                    lat: 65.5274,
+                    lng: 27.1588,
                 }
+            };
+
+            if(data.settings) {
+                settings = parsed_data.settings;
             }
+
+            var map = new google.maps.Map(document.getElementById('marker_map'), settings);
+
+            data.markers.forEach(function (marker_data) {
+
+                marker_data.map = map;
+                var marker = new google.maps.Marker(marker_data);
+
+                if(marker_data.show_infowindow == true) {
+
+                    var infowindow = new google.maps.InfoWindow({
+                        content: marker_data.infowindow_text
+                    });
+
+                    marker.addListener('click', function () {
+                        infowindow.open(map, marker);
+                    });
+
+                    if(marker_data.infowindow_open_start == true) {
+                        infowindow.open(map, marker);
+                    }
+                }
+            });
         });
+    }
+
+    $(window).hashchange(initMap);
+
+    $(document).ready(function () {
+        initMap();
     });
-}
-
-$(window).hashchange(initMap);
-
-$( document ).ready(function() {
-    initMap();
-});
-
+})()
 
 
 
