2820f849759e94b422d97869fb8e50a14bee11ac
retropikzel <retropikzel@mailbox.org>
2020-02-06 15:53:20 +0200

    [ADD] New module

---
+++ website_footer_three_columns/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Website Footer Three Columns',
+    'summary': '',
+    'author': 'Oy Tawasta OS Technologies Ltd.',
+    'license': 'AGPL-3',
+    'website': 'http://www.tawasta.fi',
+    'category': 'Website',
+    'version': '12.0.1.0.0',
+    'depends': [
+        'website',
+    ],
+    'data': [
+        'views/footer.xml',
+    ],
+    'demo': [],
+}
