04a607be46b8e80ac42f443e77233c5a3c2d9de4
retropikzel <retropikzel@mailbox.org>
2020-02-06 15:42:35 +0200

    [ADD] New module

---
+++ website_hide_copyright_footer/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Website Hide Copyright Footer',
+    'summary': '',
+    'description': '',
+    'author': 'Oy Tawasta OS Technologies Ltd.',
+    'license': 'AGPL-3',
+    'website': 'http://www.tawasta.fi',
+    'category': 'Website',
+    'version': '12.0.1.0.0',
+    'depends': [
+        'website',
+    ],
+    'data': [
+        'views/assets.xml',
+    ],
+    'demo': [],
+}
