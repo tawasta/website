c48ad2d5ba060b95a459cfdd8f0a2ae29ebd6d0e
retropikzel <retropikzel@mailbox.org>
2020-02-11 12:41:05 +0200

    [ADD] New module

---
+++ website_footer_social_icons/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Website Footer Social Icons',
+    'summary': 'Adds social links to web site footer',
+    'author': 'Oy Tawasta OS Technologies Ltd.',
+    'license': 'AGPL-3',
+    'website': 'http://www.tawasta.fi',
+    'category': 'Website',
+    'version': '12.0.1.0.0',
+    'depends': [
+        'website',
+    ],
+    'data': [
+        'views/assets.xml',
+        'views/menu.xml',
+    ],
+    'demo': [],
+}
