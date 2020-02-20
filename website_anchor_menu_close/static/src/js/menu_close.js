54901a4840708bd501992423c765574987cb6bc5
retropikzel <retropikzel@mailbox.org>
2020-02-20 10:11:32 +0200

    [FIX] Close both menus

--- website_anchor_menu_close/static/src/js/menu_close.js
+++ website_anchor_menu_close/static/src/js/menu_close.js
@@ -2,5 +2,6 @@
 (function() {
     $(window).on("hashchange", function() {
         $("#top_menu_collapse").removeClass("show");
+        $("#top_menu_collapse_clone").removeClass("show");
     });
 })();
