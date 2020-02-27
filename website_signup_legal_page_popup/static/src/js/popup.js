da22ed75de0182470b9a5f825715ab30de459df2
retropikzel <retropikzel@mailbox.org>
2020-02-27 12:51:32 +0200

    [MIG] Bringing module from 10.0

---
+++ website_signup_legal_page_popup/static/src/js/popup.js
@@ -0,0 +1,11 @@
+$('.signup-pop-print-advice').click(function() {
+    $("#print-advice").print();
+});
+
+$('.signup-pop-print-privacy').click(function() {
+    $("#print-privacy").print();
+});
+
+$('.signup-pop-print-terms').click(function() {
+    $("#print-terms").print();
+});
