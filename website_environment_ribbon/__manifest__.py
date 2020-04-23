e22c4f1f25a306630419a358125e6f99341c0c28
retropikzel <retropikzel@mailbox.org>
2020-04-23 14:44:07 +0300

    [ADD] New module

---
+++ website_environment_ribbon/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "website_gallery_mobile_fix",
+    "summary": "Fixes odoo website gallery mobile view",
+    "description": """
+        - Fix images having nospace between in gallery grid mode in mobile""",
+    "author": "Tawasta",
+    "license": "AGPL-3",
+    "website": "http://www.tawasta.fi",
+    "category": "Website",
+    "version": "12.0.1.0.0",
+    "depends": ["web", "website", "web_environment_ribbon"],
+    "data": ["views/assets_frontend.xml"],
+    "demo": [],
+}
