28b819961197ae8a7e88c25818663593b9a158c2
retropikzel <retropikzel@mailbox.org>
2020-04-23 15:14:28 +0300

    [FIX] Make ribbon bigger

--- website_environment_ribbon/static/src/js/ribbon.js
+++ website_environment_ribbon/static/src/js/ribbon.js
@@ -2,4 +2,6 @@ $( document ).ready(function () {
     var ribbon = $('<div class="test-ribbon hidden"/>');
     $('body').append(ribbon);
     ribbon.html("TEST");
+    ribbon.css("height", "64px");
+    ribbon.css("line-height", "40px");
 });
