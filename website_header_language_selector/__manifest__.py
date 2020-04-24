0fafd1c5f2d004ab94abe10f48286031c0be6116
Jarmo Kortetjärvi <jarmo.kortetjarvi@gmail.com>
2020-04-24 11:27:39 +0300

    [ADD] An option to use language code as language dropdown

--- website_header_language_selector/__manifest__.py
+++ website_header_language_selector/__manifest__.py
@@ -21,7 +21,7 @@
 {
     "name": "Website header language selector",
     "summary": "Language selector for website header",
-    "version": "12.0.1.0.0",
+    "version": "12.0.1.1.0",
     "category": "Website",
     "website": "https://github.com/Tawasta/website",
     "author": "Tawasta",
