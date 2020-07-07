fdd6ae0862cc8fda0a00bff181f7c478e4494fad
retropikzel <retropikzel@mailbox.org>
2020-07-07 13:22:47 +0300

    [FIX] JS

--- website_slides_tags_search/static/src/js/tags.js
+++ website_slides_tags_search/static/src/js/tags.js
@@ -3,17 +3,15 @@ odoo.define('website_slides_tags_search.multiple_select', function (require) {
 
     var _t = require('web.core')._t;
 
-    $(function () {
+    $(function() {
         $('#tags-select').select2({
             placeholder: _t("Tags:"),
             allowClear: true,
         });
 
-        $('#tags-select').on('change', function(){
+        $('#tags-select').on('change', function() {
             $('#tags').val($('#tags-select').val().toString());
         });
-
-        console.log("Hello");
     });
 });
 
