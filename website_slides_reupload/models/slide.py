1831eadae0635f7915c9fc4ed7cfe0eb23727449
Jarmo Kortetjärvi <jarmo.kortetjarvi@gmail.com>
2020-10-06 11:38:10 +0300

    [ADD] New module website_slides_reupload
    Allow re-uploading files to slides

---
+++ website_slides_reupload/models/slide.py
@@ -0,0 +1,19 @@
+from odoo import api
+from odoo import fields
+from odoo import models
+
+
+class Slide(models.Model):
+
+    _inherit = "slide.slide"
+
+    datas_filename = fields.Char("Slide filename")
+
+    @api.multi
+    def write(self, values):
+        if values.get("datas") and not values.get("index_content"):
+            # If the file is changed, remove incorrect indexed content
+            # TODO: recreate indexed content
+            values["index_content"] = False
+
+        return super(Slide, self).write(values)
