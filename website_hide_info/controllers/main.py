b2ba9c41a687988a55a77d460925d20451b08aae
Valtteri Lattu <36470068+valtterilattu@users.noreply.github.com>
2021-02-17 09:11:25 +0200

    12.0 dev (#172)
    * [ADD] New module to customize frontend settings
    
    * [FIX] Removes entire div for social media links
    
    * [FIX] Fixes related view as default
    
    * [FIX] Reverted back
    
    * [FIX] show related field, dont show most_viewed
    
    * [ADD] Adds read more field to blog post
    
    * manifest updated
    
    * [FIX] wrong folder ..
    
    * Created module
    
    * Created module
    
    Co-authored-by: Miika <miika@miikanissi.com>

---
+++ website_hide_info/controllers/main.py
@@ -0,0 +1,10 @@
+from odoo import http
+from odoo.http import request
+from odoo.addons.website.controllers import main
+
+
+class WebsiteHideInfo(main.Website):
+    @http.route('/website/info', type='http', auth="user", website=True)
+    def website_info(self):
+        super(WebsiteHideInfo, self).website_info()
+        return request.render('website.404')
