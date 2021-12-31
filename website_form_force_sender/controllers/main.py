d26dcfeda711ebc5adeeb6436c0ff331031fbfd3
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2021-12-31 11:05:08 +0200

    [ADD] New module website_form_force_sender
    Allow overriding the sender for website forms

---
+++ website_form_force_sender/controllers/main.py
@@ -0,0 +1,13 @@
+from odoo.addons.website_form.controllers.main import WebsiteForm
+
+
+class WebsiteFormSender(WebsiteForm):
+    def insert_record(self, request, model, values, custom, meta=None):
+        model_name = model.sudo().model
+        if model_name == "mail.mail" and request.env.company.email:
+            values.update({"reply_to": values.get("email_from")})
+
+            # Static from company mail. This could also be fetched from e.g. system parameters for more control
+            values.update({"email_from": request.env.company.email})
+
+        return super().insert_record(request, model, values, custom, meta)
