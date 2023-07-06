e70d433ca399276473de7b0d78588ffba9f57056
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2023-07-06 08:51:02 +0300

    Added logs

--- website_channel_messages/controllers/main.py
+++ website_channel_messages/controllers/main.py
@@ -233,6 +233,8 @@ class WebsiteChannelMessagesController(http.Controller):
 
         recipient_domain = self.get_recipient_domain()
         logging.info(recipient_domain);
+        test_partners = request.env["res.users"].sudo().search([]).mapped("partner_id")
+        logging.info(test_partners);
         partners = (
             request.env["res.users"]
             .sudo()
