bcc50c700baad99f4c279188523277ab5df2c247
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2023-08-24 16:40:46 +0300

    Modified class

--- website_team_snippet/static/src/js/team.js
+++ website_team_snippet/static/src/js/team.js
@@ -18,7 +18,7 @@ odoo.define("website_team_snippet.team", function (require) {
                         html += `<div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
                         <div class="card rounded shadow">
         <div class="card-body p-4"><img src="data:image/png;base64,${team.image}" alt="" class="img-fluid d-block mx-auto mb-3">
-          <div class="p-4">
+          <div class="p-3">
             <h5 class="mb-0">${team.name}</h5>
             <p class="small text-muted">${team.professional_title}</p>
             <p>${team.description}</p>
