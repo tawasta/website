9a3789479248263fcca47c8f9a0ec0412ae2ab5d
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2023-09-04 07:57:50 +0300

    Modified styles

--- website_references_snippet/static/src/js/references.js
+++ website_references_snippet/static/src/js/references.js
@@ -16,7 +16,7 @@ odoo.define("website_references_snippet.references", function (require) {
                     let html = ``;
                     data.forEach((reference) => {
                         html += `<div class="pt16 pb16 o_colored_level col-lg-2">
-                                    <div class="img img-fluid mx-auto">
+                                    <div class="img img-fluid mx-auto" style="display: flex; justify-content: center; align-items: center; height: 100%;  /* tai mikä tahansa korkeus */">
                                         <img class="img img-fluid mx-auto" src="data:image/png;base64,${reference.image}"/>
                                     </div>
                             </div>`;
