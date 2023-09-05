d7f31d46989f00f09e9b7752526b878f828aef92
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2023-09-05 14:01:38 +0300

    Removed logs

--- website_references_snippet/static/src/js/references.js
+++ website_references_snippet/static/src/js/references.js
@@ -7,17 +7,13 @@ odoo.define("website_references_snippet.references", function (require) {
         selector: ".references",
         start() {
             const referencesRow = this.el.querySelector("#res-references-row");
-            console.log("========TEST=======");
-            console.log(referencesRow);
             if (referencesRow) {
-                console.log(referencesRow);
                 this._rpc({
                     route: "/get_references/",
                     params: {},
                 }).then((data) => {
                     let html = ``;
                     data.forEach((reference) => {
-                        console.log(reference);
                         html += `<div class="pt16 pb16 o_colored_level col-lg-2">
                                     <div class="img img-fluid mx-auto" style="display: flex; justify-content: center; align-items: center; height: 100%;  /* tai mikä tahansa korkeus */">
                                         <img class="img img-fluid mx-auto" src="data:image/png;base64,${reference.image}"/>
