aa398303c2a60524f4a870548b70c80da5bf031f
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2023-09-05 12:09:06 +0300

    Added logs

--- website_references_snippet/controllers/references.py
+++ website_references_snippet/controllers/references.py
@@ -1,9 +1,12 @@
 from odoo import http
 from odoo.http import request
+import logging
 
 
 class References(http.Controller):
     @http.route("/references/", auth="public", type="json", methods=["POST"])
     def all_references(self):
         references = request.env["res.references"].search_read([], ["name", "image"])
+        logging.info("=======REFERENCES===========");
+        logging.info(references);
         return references
--- website_references_snippet/static/src/js/references.js
+++ website_references_snippet/static/src/js/references.js
@@ -9,12 +9,14 @@ odoo.define("website_references_snippet.references", function (require) {
             const referencesRow = this.el.querySelector("#res-references-row");
 
             if (referencesRow) {
+                console.log(referencesRow);
                 this._rpc({
                     route: "/references/",
                     params: {},
                 }).then((data) => {
                     let html = ``;
                     data.forEach((reference) => {
+                        console.log(reference);
                         html += `<div class="pt16 pb16 o_colored_level col-lg-2">
                                     <div class="img img-fluid mx-auto" style="display: flex; justify-content: center; align-items: center; height: 100%;  /* tai mikä tahansa korkeus */">
                                         <img class="img img-fluid mx-auto" src="data:image/png;base64,${reference.image}"/>
