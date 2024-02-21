3c8e3f1fdefaf7c5c9beb2859c3ca4778acd2c29
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2024-02-21 10:59:28 +0200

    Modified function

--- website_account_request/static/src/js/account_request.js
+++ website_account_request/static/src/js/account_request.js
@@ -1,39 +1,39 @@
-odoo.define('website_account_request.snippet', function(require) {
-    'use strict';
+odoo.define("website_account_request.snippet", function (require) {
+    "use strict";
 
-    var publicWidget = require('web.public.widget');
-    var ajax = require('web.ajax');
-    var core = require('web.core');
-    var _t = core._t;
+    var publicWidget = require("web.public.widget");
+    var ajax = require("web.ajax");
     var loadingScreen = require("website_utilities.loader").loadingScreen;
 
     publicWidget.registry.accountRequestForm = publicWidget.Widget.extend({
-        selector: '.s_account_request_form',
+        selector: ".s_account_request_form",
         events: {
-            'submit': '_onSubmit',
+            submit: "_onSubmit",
         },
 
-        _onSubmit: function(ev) {
+        _onSubmit: function (ev) {
             ev.preventDefault();
             var $form = $(ev.currentTarget);
-            var $email = $form.find('#email');
-            var $button = $form.find('button[type="submit"]');
+            var $email = $form.find("#email");
             var emailVal = $email.val();
             loadingScreen();
-            ajax.jsonRpc('/account/request', 'call', {
-                'email': emailVal,
-            }).then(function(result) {
+            ajax.jsonRpc("/account/request", "call", {
+                email: emailVal,
+            }).then(function (result) {
                 if (result.success) {
-                    var loginInfoHtml = "<p>Your account request has been processed successfully.</p>" +
-                                        "<p><b>Login:</b> " + result.login + "</p>" +
-                                        "<p><b>Password:</b> " + result.password + "</p>" +
-                                        "<p>With these credentials, you can log into the demo installation. " +
-                                        "<p><a href='/web/login' class='btn btn-primary'>Log In</a></p>";
+                    var loginInfoHtml =
+                        "<p>Your account request has been processed successfully.</p>" +
+                        "<p><b>Login:</b> " +
+                        result.login +
+                        "</p>" +
+                        "<p><b>Password:</b> " +
+                        result.password +
+                        "</p>" +
+                        "<p>With these credentials, you can log into the demo installation. " +
+                        "<p><a href='/web/login' class='btn btn-primary'>Log In</a></p>";
 
-
-
-                    $('#requestSuccessModal .modal-body').html(loginInfoHtml);
-                    $('#requestSuccessModal').modal('show');
+                    $("#requestSuccessModal .modal-body").html(loginInfoHtml);
+                    $("#requestSuccessModal").modal("show");
                     $form[0].reset();
                 } else {
                     // Näytä virheviesti
