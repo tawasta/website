433ab9be920ddb88a6f172b07e8d5772c3cbbafe
difbit <timo.kekalainen@tawasta.fi>
2024-06-19 13:37:10 +0300

    [MIG] Migration of portal_details_street2 module from 14.0 to 17.0 version

---
+++ portal_details_street2/controllers/portal.py
@@ -0,0 +1,6 @@
+from odoo.addons.portal.controllers.portal import CustomerPortal
+
+
+class CustomerPortalDetails(CustomerPortal):
+    # Optional fields are defined in a list OPTIONAL_BILLING_FIELDS
+    CustomerPortal.OPTIONAL_BILLING_FIELDS.append("street2")
