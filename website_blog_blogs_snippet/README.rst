f9ec6830c8a445721a6848f9d85260f9378beb76
difbit <timo.kekalainen@tawasta.fi>
2024-08-07 13:15:43 +0300

    [MIG] Migration of website_blog_blogs_snippet module from 14.0 to 17.0 version

---
+++ website_blog_blogs_snippet/README.rst
@@ -0,0 +1,41 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+==========================
+Website Blog Blogs Snippet
+==========================
+* New Improved Snippet for Blog Posts "Squares"
+* New Improved Snippet for Blog Posts "List box"
+* Ability to select number of columns per row for each layout
+* Ability to only show promoted blog posts in snippets
+
+Configuration
+=============
+\-
+
+Usage
+=====
+\-
+
+Known issues / Roadmap
+======================
+\-
+
+Credits
+=======
+
+Contributors
+------------
+
+* Miika Nissi <miika.nissi@tawasta.fi>
+* Timo Kekäläinen <timo.kekalainen@tawasta.fi>
+
+Maintainer
+----------
+
+.. image:: http://tawasta.fi/templates/tawastrap/images/logo.png
+   :alt: Oy Tawasta OS Technologies Ltd.
+   :target: http://tawasta.fi/
+
+This module is maintained by Oy Tawasta OS Technologies Ltd.
---
+++ website_blog_blogs_snippet/__manifest__.py
@@ -0,0 +1,38 @@
+##############################################################################
+#
+#    Author: Oy Tawasta OS Technologies Ltd.
+#    Copyright 2022- Oy Tawasta OS Technologies Ltd. (http://www.tawasta.fi)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see http://www.gnu.org/licenses/agpl.html
+#
+##############################################################################
+{
+    "name": "Website Blog Blogs Snippet",
+    "summary": "Improved Blogs Snippets",
+    "version": "17.0.1.0.0",
+    "category": "Website",
+    "website": "https://gitlab.com/tawasta/odoo/website",
+    "author": "Tawasta",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["web", "website_blog"],
+    "data": ["views/website_blog_views.xml", "views/snippets/s_latest_posts.xml"],
+    "assets": {
+        "web.assets_frontend": [
+            "/website_blog_blogs_snippet/static/src/js/s_latest_posts.esm.js",
+            "/website_blog_blogs_snippet/static/src/scss/s_latest_posts.scss",
+        ]
+    },
+}
