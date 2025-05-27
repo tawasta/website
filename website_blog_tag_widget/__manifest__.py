f89196f9b487e58935a607bf6c3624d26530caef
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2025-05-27 07:53:22 +0300

    [IMP] - created new module

---
+++ website_blog_tag_widget/__manifest__.py
@@ -0,0 +1,44 @@
+##############################################################################
+#
+#    Author: Oy Tawasta OS Technologies Ltd.
+#    Copyright 2022- Oy Tawasta OS Technologies Ltd. (http://www.tawasta.fi)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see http://www.gnu.org/licenses/agpl.html
+#
+##############################################################################
+{
+    "name": "Website Blog Tag Widget",
+    "summary": "Website",
+    "version": "17.0.1.0.1",
+    "category": "Website",
+    "website": "https://gitlab.com/tawasta/odoo/website",
+    "author": "Tawasta",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["website", "website_snippet_filter_core"],
+    "data": [
+        "data/tagcloud_snippet_template_data.xml",
+        "views/snippets/s_tagcloud.xml",
+        "views/snippets/snippets.xml",
+    ],
+    "assets": {
+        "web.assets_frontend": [
+            "/website_blog_tag_widget/static/src/scss/main.scss",
+        ],
+        "website.assets_wysiwyg": [
+            "/website_blog_tag_widget/static/src/snippets/s_tagcloud/options.esm.js",
+        ],
+    },
+}
