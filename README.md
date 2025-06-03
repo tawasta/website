043a922b51eb3c78e3ba4e29b1ee62092cf33168
Timo Talvitie <timo.talvitie@tawasta.fi>
2025-06-03 17:47:52 +0300

    [ADD] new module website_blog_tag_html_description

--- README.md
+++ README.md
@@ -28,6 +28,7 @@ addon | version | maintainers | summary
 [website_blog_settings](website_blog_settings/) | 17.0.1.0 |  | Customization settings under website frontend customize menu
 [website_blog_snippet_extra_options](website_blog_snippet_extra_options/) | 17.0.1.0.0 |  | Additional configurations for the core snippet
 [website_blog_snippet_template_clean](website_blog_snippet_template_clean/) | 17.0.1.0.1 |  | Website
+[website_blog_tag_html_description](website_blog_tag_html_description/) | 17.0.1.0.0 |  | New field, shown in frontend tag page when only single tag is selected
 [website_blog_tag_tag_specific_styling](website_blog_tag_tag_specific_styling/) | 17.0.1.0.0 |  | Add classes to blog-related DOM elements, to be then styled with website builder's CSS editor
 [website_blog_tag_widget](website_blog_tag_widget/) | 17.0.1.0.1 |  | Website
 [website_crm_contact_form_select_based_lead_name](website_crm_contact_form_select_based_lead_name/) | 17.0.1.0.0 |  | Allows configuring web leads' subject to be based on a selection list instead of freetext
---
+++ website_blog_tag_html_description/controllers/main.py
@@ -0,0 +1,46 @@
+from odoo.addons.website_blog.controllers.main import WebsiteBlog
+from odoo.http import request
+
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+class WebsiteBlogSequence(WebsiteBlog):
+    def _prepare_blog_values(
+        self,
+        blogs,
+        blog=False,
+        date_begin=False,
+        date_end=False,
+        tags=False,
+        state=False,
+        page=False,
+        search=None,
+        **post,
+    ):
+        # Check if the generic blog view is being used to show the contents of a
+        # single tag. If yes, pass the tag object so that its name and html description
+        # will be rendered in frontend
+
+        res = super()._prepare_blog_values(
+            blogs=blogs,
+            blog=blog,
+            date_begin=date_begin,
+            date_end=date_end,
+            tags=tags,
+            state=state,
+            page=page,
+            search=search,
+            **post,
+        )
+
+        if len(res["active_tag_ids"]) == 1:
+            res["show_tag_html_description"] = True
+            res["active_tag_id"] = request.env["blog.tag"].search(
+                [("id", "=", res["active_tag_ids"][0])]
+            )
+        else:
+            res["show_tag_html_description"] = False
+
+        return res
