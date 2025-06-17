e27343d6bc7177a776136c4c62a67ba5bc0a5e59
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2025-06-17 14:15:30 +0300

    [IMP] - modified function

--- signup_url_override/models/res_partner.py
+++ signup_url_override/models/res_partner.py
@@ -7,42 +7,36 @@ _logger = logging.getLogger(__name__)
 try:
     from odoo.http import request
 except ImportError:
-    request = None  # Ei HTTP-kontekstia
+    request = None
+
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     def _compute_signup_url(self):
-        result = self.sudo()._get_signup_url_for_action()
+        # Call the original Odoo implementation first
+        super()._compute_signup_url()
 
-        referer = None
-        request_path = None
-
-        if request:
-            referer = request.httprequest.headers.get('Referer', '')
-            request_path = request.httprequest.path
-            _logger.info("Signup URL computed from HTTP path: %s, referer: %s", request_path, referer)
-        else:
+        # Now apply custom logic if request context is available
+        if not request:
             _logger.info("Signup URL computed outside HTTP context")
+            return
+
+        referer = request.httprequest.headers.get('Referer', '')
+        request_path = request.httprequest.path
+        _logger.info("Signup URL computed from HTTP path: %s, referer: %s", request_path, referer)
 
         for partner in self:
-            if any(u._is_internal() for u in partner.user_ids if u != self.env.user):
-                self.env['res.users'].check_access_rights('write')
-            if any(u.has_group('base.group_portal') for u in partner.user_ids if u != self.env.user):
-                self.env['res.partner'].check_access_rights('write')
-
-            signup_url = result.get(partner.id, False)
-
+            signup_url = partner.signup_url
             if signup_url and request_path and '/reset_password' in request_path and '/reset_password' in signup_url and referer:
                 parsed_referer = werkzeug.urls.url_parse(referer)
                 parsed_signup = werkzeug.urls.url_parse(signup_url)
 
-                # Tarkista että domainit eroavat
+                # Tarkistetaan, ovatko domainit eriäviä
                 if parsed_signup.host != parsed_referer.host:
-                    # Korvaa domain signup_urlissa refererillä
+                    # Korvataan domain signup_url:ssä refererin domainilla
                     new_signup_url = parsed_signup.replace(netloc=parsed_referer.netloc).to_url()
                     _logger.info("Overriding domain in signup URL due to referer mismatch: %s -> %s", signup_url, new_signup_url)
-                    signup_url = new_signup_url
+                    partner.signup_url = new_signup_url
 
-            _logger.info("Generated signup URL for partner [%s - %s]: %s", partner.id, partner.name, signup_url)
-            partner.signup_url = signup_url
+            _logger.info("Final signup URL for partner [%s - %s]: %s", partner.id, partner.name, partner.signup_url)
