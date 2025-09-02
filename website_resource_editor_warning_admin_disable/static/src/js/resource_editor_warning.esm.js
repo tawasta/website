f6a9d5e47128baadfe2c4bb7578088d11f470183
joona-isoaho-futural <joona.isoaho@futural.fi>
2025-09-02 07:02:37 +0300

    Precommit fixes

--- website_resource_editor_warning_admin_disable/static/src/js/resource_editor_warning.esm.js
+++ website_resource_editor_warning_admin_disable/static/src/js/resource_editor_warning.esm.js
@@ -2,7 +2,7 @@
 
 import {session} from "@web/session";
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
     if (session.is_admin === true || session.is_system === true) {
         window.localStorage.setItem("website.ace.doNotShowWarning", "true");
     }
