0c6edeb957422df8273b417e82f0e44bc0ec65b3
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2026-01-23 09:19:45 +0200

    [IMP] - modules to production branch

---
+++ website_visibility_slides/models/slide.py
@@ -0,0 +1,39 @@
+import logging
+
+from odoo import fields, models
+from odoo.http import request
+from odoo.osv import expression
+
+_logger = logging.getLogger(__name__)
+
+
+class SlideChannel(models.Model):
+    _inherit = "slide.channel"
+
+    visible_website_ids = fields.Many2many(
+        "website",
+        string="Visible on Websites",
+        index=True,
+    )
+
+    def can_access_from_current_website(self, website_id=False):
+        current_id = website_id or request.env["website"].get_current_website().id
+        for rec in self:
+            if (
+                rec.visible_website_ids
+                and current_id not in rec.visible_website_ids.ids
+            ):
+                return False
+        return True
+
+    def search(self, args, **kwargs):
+        wid = self.env.context.get("website_id")
+        if wid:
+            dom = expression.OR(
+                [
+                    [("visible_website_ids", "=", False)],
+                    [("visible_website_ids", "in", [wid])],
+                ]
+            )
+            args = expression.AND([args, dom])
+        return super().search(args, **kwargs)
