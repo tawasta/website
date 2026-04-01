adc484b1f8173eda8f4d2d7fe07f74b04e5caade
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2026-04-01 07:41:33 +0300

    [IMP] - modified domain

--- website_blog_view_domain/models/blog.py
+++ website_blog_view_domain/models/blog.py
@@ -19,17 +19,19 @@ class BlogBlog(models.Model):
         if not wid:
             return []
 
-        return [
-            "|", "|",
-            ("website_id", "=", False),
-            ("website_id", "=", wid),
-            ("visible_website_ids", "in", [wid]),
-        ]
+        if self.env.user.has_group("website.group_website_manager"):
+            return [
+                "|",
+                "|",
+                ("website_id", "=", False),
+                ("website_id", "=", wid),
+                ("visible_website_ids", "in", [wid]),
+            ]
+
+        return [("website_id", "=", wid)]
 
     def _apply_website_filter(self, domain):
         domain = domain or []
-        if self.env.user.has_group("website.group_website_manager"):
-            return domain
         wdom = self._website_filter_domain()
         return expression.AND([domain, wdom]) if wdom else domain
 
