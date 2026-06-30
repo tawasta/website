7d6d56e611fe143a0ae840b80ad51889e53c06c6
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2026-06-30 10:14:40 +0300

    [IMP]  modified number of records value

--- website_references_snippet/models/website_snippet_filter.py
+++ website_references_snippet/models/website_snippet_filter.py
@@ -4,6 +4,11 @@ from odoo import _, models
 class WebsiteSnippetFilter(models.Model):
     _inherit = "website.snippet.filter"
 
+    def _get_model_max_limit(self, model_name):
+        if model_name == "blog.tag":
+            return 60
+        return super()._get_model_max_limit(model_name)
+
     def _get_hardcoded_sample(self, model):
         if model._name != "res.references":
             return super()._get_hardcoded_sample(model)
